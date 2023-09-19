40d67efe6d6810c8bf07eb1ca752e0bf259d922c
Noah Salmon <noahsalmon98@gmail.com>
2023-09-19 14:15:38 -0400

    added thought and reaction schema

---
+++ models/Thought.js
@@ -0,0 +1,19 @@
+const { Schema, model } = require("mongoose");
+
+const thoughtSchema = new Schema({
+  thoughtText: {
+    type: String,
+    required: true,
+    minLength: 1,
+    maxLength: 280,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  reactions: [{ type: Schema.Types.ObjectId, ref: "reaction" }],
+});
